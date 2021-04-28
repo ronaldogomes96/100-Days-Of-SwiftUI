2845a5b0fc201cb19a07a361919ad02cb4e24ce6
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-04-28 19:08:33 -0300

    Project using lists and navigation, with string and folders manipulation:

--- 100 Day of SwiftUI/Project05/WordScramble/WordScramble/ContentView.swift
+++ 100 Day of SwiftUI/Project05/WordScramble/WordScramble/ContentView.swift
@@ -8,71 +8,122 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Finn", "Leia", "Luke", "Rey"]
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
-        // List cria celulas na tela
-        List {
-            Text("Hello World")
-            Text("Hello World")
-            Text("Hello World")
-        }
-        //Faz ficar com estilo mais parecido com form
-        .listStyle(GroupedListStyle())
-        
-        List {
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
-            }
-        }
-        
-        List {
-            Text("Static row 1")
-            Text("Static row 2")
-
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
-            }
-
-            Text("Static row 3")
-            Text("Static row 4")
-        }
-        
-        // Podemos fazer list com sections, porem fica mais bonita com form
-        List {
-            Section(header: Text("Section 1")) {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-
-            Section(header: Text("Section 2")) {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
+        NavigationView {
+            VStack {
+                // OnCommit é chamado quando pressiona enter
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                
+                // Lista dinamica
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
                 }
             }
+            .navigationBarTitle(rootWord)
+            .onAppear(perform: startGame)
+            .navigationBarItems(leading:
+                                    Button("Start Game"){
+                                        startGame()
+                                    })
+            
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    func addNewWord() {
+        // Transforma para minuscula e retira espacos no comeco e fim, para ter certeza de não adicionar palavras duplicadas com diferenças de maiúsculas e minúsculas
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Verifica se a string é vazia
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // Algumas validacoes
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
 
-            Section(header: Text("Section 3")) {
-                Text("Static row 3")
-                Text("Static row 4")
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame() {
+        // Achar a url do arquivo txt
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // Carregar o conteudo do arquivo em uma string
+            if let startWords = try? String(contentsOf: startWordURL) {
+                // Quebrar em um array de palavras
+                let allWords = startWords.components(separatedBy: "\n")
+                
+                // Escolhendo uma palavra aleatoriamente
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
+                return
             }
         }
         
-        //Criando as list sem for each, totalmente dinamica
-        List(0..<5) {
-            Text("Dynamic row \($0)")
-        }
-        
-        //Podemos usar o id:./ para referenciar array de dados
-        List(people, id: \.self) {
-            Text($0)
-        }
-        
-        //Foreach funciona da mesma forma
-        List {
-            ForEach(people, id: \.self) {
-                Text($0)
+        // Se acontece algum erro
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
             }
         }
+
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        if word.count < 3 || word == rootWord {
+            return false
+        }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
