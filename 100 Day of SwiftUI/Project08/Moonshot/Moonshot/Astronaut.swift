df5271342196f97eede3e15cae77844c3a30f5a4
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-05-16 00:23:06 -0300

    Implementation of project 98, with three views, generics, codables, data, layouts, stacks

---
+++ 100 Day of SwiftUI/Project08/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ronaldo Gomes on 15/05/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- 100 Day of SwiftUI/Project08/Moonshot/Moonshot/ContentView.swift
+++ 100 Day of SwiftUI/Project08/Moonshot/Moonshot/ContentView.swift
@@ -8,37 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decodeForAstronautArray("astronauts.json")
+    let missions: [Mission] = Bundle.main.decodeForAstronautArray("missions.json")
+    
     var body: some View {
         NavigationView {
-            List(0..<100) { row in
-                // Chama a proxima tela
-                NavigationLink(destination: Text("Detail \(row)")) {
-                    Text("Row \(row)")
+            List(missions) { mission in
+                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
                 }
             }
-            .navigationBarTitle("SwiftUI")
-        }
-        
-        VStack {
-            // Funciona como um adaptador do tamanho da tela pra imagem. Dessa forma ela fica com aspecto correto nao importa o tamanho da tela. Ele automaticamente tem o width e height
-            GeometryReader { geo in
-                Image("Example")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
-            }
-        }
-        
-        // Exemplo de scrollView
-        ScrollView(.vertical) {
-            // A vstack cria todas as instancias da classe automaticamente, ja o List nao
-            VStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
-                }
-            }
-            .frame(maxWidth: .infinity)
+            .navigationBarTitle("Moonshot")
         }
     }
 }
@@ -47,20 +36,6 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
-            ContentView()
         }
-    }
-}
-
-struct CustomText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
     }
 }
