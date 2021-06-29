70c9887834e98c0c3799dd29c15b7d31fea3fa0a
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-06-29 20:58:54 -0300

    Add examples for CGAffineTransform and ImagePaint in drawing project

--- 100 Day of SwiftUI/Project09/Drawing/Drawing/ContentView.swift
+++ 100 Day of SwiftUI/Project09/Drawing/Drawing/ContentView.swift
@@ -8,7 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 50.0
+    
     var body: some View {
+        Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+            .fill(Color.red, style: FillStyle(eoFill: true))
+        
         // Cria / desenha um retangulo na tela
 //        Path { path in
 //            path.move(to: CGPoint(x: 200, y: 100))
@@ -34,6 +40,11 @@ struct ContentView: View {
         Circle()
             // Desenha a borda dentro da tela
             .strokeBorder(Color.blue, lineWidth: 40)
+        
+        // Com o imagePaint, temos mais controle de bordas de uma view, passando um determinado scale
+        Text("Hello World")
+            .frame(width: 300, height: 300)
+            .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
     }
 }
 
@@ -68,6 +79,41 @@ struct Arc: Shape {
         var path = Path()
         path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
 
+        return path
+    }
+}
+
+// Cria uma shape de flor
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+
+    // How wide to make each petal
+    var petalWidth: Double = 100
+
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            // move the petal to be at the center of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
+
+            // apply our rotation/position transformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+
+        // now send the main path back
         return path
     }
 }
